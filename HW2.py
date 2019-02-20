94026a36c276dec40b3a04d22ea0e426638ec390
n8th-wonder <45256739+n8th-wonder@users.noreply.github.com>
2019-02-20 01:43:25 -0500

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,11 @@
+def solver(inp): #Solves the 8-puzzle
+
+
+def generator():  ##Generates a random 8-puzzle state
+[[1,2,3],[0,4,5],[6,8,7]]
+def tester(): ##tests the solver
+
+def construct_tree(lst):
+    malformed = False
+    if (malformed):
+        return "INVALID INPUT"
